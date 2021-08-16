403d6b09de7fa8693814ed36c8e6b6ace41b40a8
hiroka1642 <74912714+hiroka1642@users.noreply.github.com>
2021-08-16 22:44:06 +0900

    ページの作成を行いました。

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrymaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecoundaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/template/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "じゃけえ",
-  image: "https://source.unsplash.com/73pyV0JJOmE",
-  email: "12345@example.com",
-  phone: "090-2121-4343",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrymaryButton children="テスト" />
-        <SecondaryButton children="検索" />
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
