bc992880d5c2a21b47cd64cd43abb481ff4d26bd
hiroka1642 <74912714+hiroka1642@users.noreply.github.com>
2021-08-16 21:47:04 +0900

    Initial commit

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { PrymaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/input";
+
+export const SearchInput = () => {
+  return (
+    <SContainer>
+      <Input placeholder="検索条件を入力" />
+      <SButtonWrapper>
+        <PrymaryButton>検索</PrymaryButton>
+      </SButtonWrapper>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
